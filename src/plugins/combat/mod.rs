a8be9cdb9c95fea5976800f7555e0773a1f8f718
Joseph LeBlanc <joseph.t.leblanc@protonmail.com>
2023-10-16 13:26:31 -0700

    first pass at turn based movement

---
+++ src/plugins/combat/mod.rs
@@ -0,0 +1,5 @@
+use bevy::prelude::*;
+
+use crate::scenes::SceneState;
+
+pub struct CombatPlugin;
--- src/plugins/input/mod.rs
+++ src/plugins/input/mod.rs
@@ -9,6 +9,7 @@ pub struct InputHandlePlugin;
 
 impl Plugin for InputHandlePlugin {
     fn build(&self, app: &mut App) {
+        app.init_resource::<movement::Movement>();
         app.add_systems(
             OnEnter(SceneState::InGameClassicMode),
             cleanup::cleanup_mouse,
@@ -33,7 +34,7 @@ impl Plugin for InputHandlePlugin {
                 // feature::use_skill,
                 // crate::scenes::pause_scene::pause,
                 // feature::use_mouse,
-                movement::player_movement_handle_system, // .after(crate::plugins::player::stats::update_stats),
+                movement::player_movement_system, // .after(crate::plugins::player::stats::update_stats),
             )
                 .run_if(
                     in_state(SceneState::InGameClassicMode), // .or_else(in_state(SceneState::InGameSurvivalMode))
