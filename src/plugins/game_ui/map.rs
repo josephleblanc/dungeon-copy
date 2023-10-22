79d43d384646be2af3c87e77d52e061519d27c87
Joseph LeBlanc <joseph.t.leblanc@protonmail.com>
2023-10-21 19:10:34 -0700

    place map grid focus box in bottom left of screen when mouse is not interacting with the grid

--- src/plugins/game_ui/map.rs
+++ src/plugins/game_ui/map.rs
@@ -1,6 +1,6 @@
 use bevy::prelude::*;
 
-use crate::config::TILE_SIZE;
+use crate::config::{RESOLUTION, TILE_SIZE, WINDOW_HEIGHT};
 use crate::materials::ingame::InGameMaterials;
 use crate::plugins::interact::Interactable;
 use crate::resources::dungeon::grid_square::GridSquare;
@@ -12,30 +12,48 @@ pub struct MapUiData {
 }
 
 #[derive(Component)]
-pub struct FocusBox;
+pub struct FocusBox {
+    hide_pos: Vec3,
+    display_z: f32,
+}
 
 pub fn mouse_handle_system(
     query_grid: Query<
         (&Transform, &Interactable),
         (With<GridSquare>, Changed<Interactable>, Without<FocusBox>),
     >,
-    mut query_focus_box: Query<&mut Transform, With<FocusBox>>,
+    mut query_focus_box: Query<(&mut Transform, &FocusBox)>,
 ) {
-    for (interact_transform, interactable) in query_grid.iter() {
-        if interactable.focused && interactable.active {
-            let mut focus_box_transform = query_focus_box.get_single_mut().unwrap();
-            focus_box_transform.translation.x = interact_transform.translation.x;
-            focus_box_transform.translation.y = interact_transform.translation.y;
+    if let Some((interact_transform, interactable)) = query_grid
+        .iter()
+        .find(|(_, interactable)| interactable.focused)
+    {
+        if interactable.active {
+            let (mut focus_box_transform, focus_box) = query_focus_box.get_single_mut().unwrap();
+            focus_box_transform.translation = (
+                interact_transform.translation.x,
+                interact_transform.translation.y,
+                focus_box.display_z,
+            )
+                .into();
+        }
+    } else {
+        let (mut focus_box_transform, focus_box) = query_focus_box.get_single_mut().unwrap();
+        if focus_box_transform.translation != focus_box.hide_pos {
+            focus_box_transform.translation = focus_box.hide_pos;
         }
     }
 }
 
 pub fn setup(mut commands: Commands, ingame_materials: Res<InGameMaterials>) {
     let user_interface_root = commands
-        .spawn(NodeBundle {
-            background_color: Color::NONE.into(),
-            ..Default::default()
-        })
+        .spawn((
+            NodeBundle {
+                background_color: Color::NONE.into(),
+                ..Default::default()
+            },
+            Name::new("Map Focus Box Root Node"),
+        ))
         .id();
     // .with_children(|builder| {
     //     build_focus_box(builder);
@@ -64,6 +82,12 @@ pub fn build_focus_box(builder: &mut ChildBuilder, ingame_materials: Res<InGameM
     // The z_layer should be between the player/monster sprites above and the
     // map tiles below.
     let z_layer = 0.125;
+
+    let hidden_offscreen = Vec3::new(
+        -1.0 * (WINDOW_HEIGHT * RESOLUTION / 2.0),
+        -1.0 * (WINDOW_HEIGHT / 2.0),
+        z_layer,
+    );
     builder.spawn((
         SpriteBundle {
             sprite: Sprite {
@@ -77,7 +101,10 @@ pub fn build_focus_box(builder: &mut ChildBuilder, ingame_materials: Res<InGameM
             texture: ingame_materials.map_ui.grid_select_box.clone(),
             ..Default::default()
         },
-        FocusBox,
+        FocusBox {
+            hide_pos: hidden_offscreen,
+            display_z: z_layer,
+        },
         Name::new("Grid Focus Box"),
     ));
 }
