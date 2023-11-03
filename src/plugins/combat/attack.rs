300135f3871c0a0ff478ec887a1ce8a443d5d499
Joseph LeBlanc <joseph.t.leblanc@protonmail.com>
2023-11-03 15:06:29 -0700

    add documentation and improve both attack_roll system and AttackRollEvent

--- src/plugins/combat/attack.rs
+++ src/plugins/combat/attack.rs
@@ -11,17 +11,28 @@ use crate::{
 
 use super::{
     armor_class::{ACBonusEvent, ACBonusSumEvent},
-    attack_modifiers::{AttackModifier, AttackModifierEvent, AttackModifierList},
+    attack_modifier::{AttackModifier, AttackModifierEvent, AttackModifierList},
     bonus::BonusType,
 };
 
 #[derive(Clone, Event)]
+
+/// AttackBonusEvent is sent by `start_attack` and listened for by all of the systems in the
+/// `attack_modifier` mod. This event is the signal for all of the systems which check to see if
+/// they can apply an attack modifier to an attack should be run.
+/// Since this event is sent by one system and listened to by many, it is important for the system
+/// scheduling to ensure that all of the systems which listen for it run after this event is sent.
+/// If not, logic errors could pop up - for example, if two attacks different attacks occured in
+/// consecutive frames, then the bonuses from one attack might try to be added to another.
 pub struct AttackBonusEvent {
     pub attacker: Entity,
     pub defender: Entity,
 }
 
 #[derive(Event)]
+/// This event is sent by `check_attack_conditions` and listened to by `start_attack`. This
+/// singleton event is the signal that all of the conditions have been met for the attack roll and
+/// damage systems to run without causing a `panic`.
 pub struct StartAttack;
 
 #[derive(Clone, Event)]
@@ -31,9 +42,11 @@ pub struct AttackBonusSumEvent {
     defender: Entity,
 }
 
-#[derive(Clone, Event)]
-pub struct AttackRollComplete();
-
+/// This is where the attack roll process begins. Once all of the conditions have been met this
+/// function will send an event that lets `start_attack` begin.
+/// Any conditions that must be met for an attack to begin should be put here, along with anything
+/// that could cause a `panic` in the systems which compose the process of attacking and applying
+/// damage.
 pub fn check_attack_conditions(
     interacting_pos: Res<InteractingPos>,
     mut attack_event_writer: EventWriter<StartAttack>,
@@ -60,7 +73,7 @@ pub fn check_attack_conditions(
 /// Then it sends two events, one to start the systems calculating the total of the attack modifiers,
 /// another to the systems calculating the ac of the defender.
 /// There are multiple systems which check whether each modifier should be applied to ac and attack,
-/// and then send an event listened to by `sum_attack_modifiers`.
+/// and then send an event listened to by `sum_attack_modifier`.
 pub fn start_attack(
     mut start_attack_events: EventReader<StartAttack>,
     mut attack_event_writer: EventWriter<AttackBonusEvent>,
@@ -68,10 +81,13 @@ pub fn start_attack(
     query_player: Query<Entity, With<ActionPriority>>,
     interacting_pos: Res<InteractingPos>,
 ) {
+    let debug = true;
     if !start_attack_events.is_empty() {
         start_attack_events.clear();
 
-        println!("debug | attack::start_attack | start");
+        if debug {
+            println!("debug | attack::start_attack | start");
+        }
 
         let attacker = query_player.get_single().unwrap();
         let defender = interacting_pos.entity.unwrap();
@@ -80,7 +96,15 @@ pub fn start_attack(
     }
 }
 
-pub fn sum_attack_modifiers(
+/// `sum_attack_modifier` adds together all of the modifiers in the `attack_modifier` mod. It
+/// listens for the event `AttackModifierEvent`, which should have been sent out by each of the
+/// systems deciding whether a modifier should be applied to the attack.
+/// Because this is a system which listens for an event which is sent out by many systems, it is
+/// important to use explicit system scheduling to ensure all of the systems in `attack_modifier`
+/// have run before this system. Otherwise some attack modifiers may not be applied to the attack
+/// which prompted the modifier system to run, and could prompt a `panic` or logical error when
+/// they are attempted to be summed with the modifiers from another attack.
+pub fn sum_attack_modifier(
     mut atk_mod_events: EventReader<AttackModifierEvent>,
     attacker_query: Query<Entity, With<ActionPriority>>,
     mut atk_mod_finished: EventWriter<AttackBonusSumEvent>,
@@ -90,7 +114,7 @@ pub fn sum_attack_modifiers(
         .map(|&event| event.into())
         .collect();
     if !atk_mod_list.is_empty() {
-        println!("debug | attack::sum_attack_modifiers | start");
+        println!("debug | attack::sum_attack_modifier | start");
         if let Ok(attacker_entity) = attacker_query.get_single() {
             let attacker = atk_mod_list.verified_attacker().unwrap();
             let defender = atk_mod_list.verified_defender().unwrap();
@@ -109,12 +133,9 @@ pub fn sum_attack_modifiers(
     }
 }
 
-pub struct DamageStartEvent {
-    attacker: Entity,
-    defender: Entity,
-}
-
 #[derive(Debug, Copy, Clone)]
+/// AttackOutcome is the enum which describes the outcome of an attack roll, with modifiers
+/// applied, against a valid target.
 pub enum AttackOutcome {
     CriticalHit,
     Hit,
@@ -123,12 +144,24 @@ pub enum AttackOutcome {
 }
 
 #[derive(Debug, Event, Copy, Clone)]
+/// AttackRollEvent is the event sent out by `attack_roll`, and includes the outcome of an attack
+/// against a valid target. This event is listened to by `start_damage`, which is the gatekeeper
+/// for the systems which calculate the attack's damage.
 pub struct AttackRollEvent {
     attacker: Entity,
     defender: Entity,
+    raw_attack_roll: usize,
+    total_attack_modifier: isize,
+    total_defender_ac: isize,
     attack_outcome: AttackOutcome,
 }
 
+/// `attack_roll` is the system which sums all of the attack modifiers and armor class modifiers,
+/// rolls the attack die, and then determines the outcome of the attack. An attack can `Hit`, `Miss`,
+/// but can also `CriticalHit` or `CriticalMiss`.
+/// If the attack is a `Hit` or a `CriticalHit`, the `damage::start_damage` system will start
+/// calculating the damage of the attack.
+// TODO: Decide how to handle the conclusion of the attack as a whole.
 pub fn attack_roll(
     mut ac_mod_finished: EventReader<ACBonusSumEvent>,
     mut atk_mod_finished: EventReader<AttackBonusSumEvent>,
@@ -143,61 +176,90 @@ pub fn attack_roll(
             ac_event.attacker == atk_event.attacker && ac_event.defender == atk_event.defender
         })
     {
+        let debug = true;
         println!("debug | attack::attack_roll | start");
         let defender = defender_query.get(ac_event.defender).unwrap();
         let (attacker, attacker_bab) = attacker_query.get(atk_event.attacker).unwrap();
 
         let mut rng = rand::thread_rng();
-        let attack_roll = Dice::D20.roll_once(&mut rng);
-        println!("      |                     | D20 roll: {}", attack_roll);
-        println!("      |                     | BAB: {}", **attacker_bab);
-        println!(
-            "      |                     | summed attack modifiers: {}",
-            atk_event.total_attack_bonus
-        );
-        println!(
-            "      |                     | attack bonus total: {}",
-            atk_event.total_attack_bonus + **attacker_bab
-        );
-        println!(
-            "      |                     | defender AC bonus: {}",
-            ac_event.total_ac_bonus
-        );
-        println!(
-            "      |                     | defender AC total: {}",
-            10 + ac_event.total_ac_bonus
-        );
+        let raw_attack_roll = Dice::D20.roll_once(&mut rng);
+        let total_attack_modifier = atk_event.total_attack_bonus + **attacker_bab;
+        let total_defender_ac = 10 + ac_event.total_ac_bonus;
 
-        let attack_roll_total: isize =
-            attack_roll as isize + atk_event.total_attack_bonus + **attacker_bab;
-        let attack_outcome = if attack_roll == 20 {
+        let attack_roll_total: isize = raw_attack_roll as isize + total_attack_modifier;
+        let attack_outcome = if raw_attack_roll == 20 {
             AttackOutcome::CriticalHit
-        } else if attack_roll == 1 {
+        } else if raw_attack_roll == 1 {
             AttackOutcome::CriticalMiss
-        } else if 10 + ac_event.total_ac_bonus <= attack_roll_total {
+        } else if total_defender_ac <= attack_roll_total {
             AttackOutcome::Hit
         } else {
             AttackOutcome::Miss
         };
-        println!(
-            "      |                     | total attack roll with bonuses: {}",
-            attack_roll_total
-        );
-        println!(
-            "      |                     | attack outcome: {:?}",
-            attack_outcome
-        );
 
         attack_roll_event_writer.send(AttackRollEvent {
             attacker,
             defender,
             attack_outcome,
+            raw_attack_roll,
+            total_attack_modifier,
+            total_defender_ac,
         });
+
+        if debug {
+            debug_attack_roll(
+                raw_attack_roll,
+                attacker_bab,
+                atk_event,
+                ac_event,
+                attack_roll_total,
+                attack_outcome,
+            );
+        }
     }
 }
 
+fn debug_attack_roll(
+    attack_roll: usize,
+    attacker_bab: &BaseAttackBonus,
+    atk_event: &AttackBonusSumEvent,
+    ac_event: &ACBonusSumEvent,
+    attack_roll_total: isize,
+    attack_outcome: AttackOutcome,
+) {
+    println!("      |                     | D20 roll: {}", attack_roll);
+    println!("      |                     | BAB: {}", **attacker_bab);
+    println!(
+        "      |                     | summed attack modifiers: {}",
+        atk_event.total_attack_bonus
+    );
+    println!(
+        "      |                     | attack bonus total: {}",
+        atk_event.total_attack_bonus + **attacker_bab
+    );
+    println!(
+        "      |                     | defender AC bonus: {}",
+        ac_event.total_ac_bonus
+    );
+    println!(
+        "      |                     | defender AC total: {}",
+        10 + ac_event.total_ac_bonus
+    );
+    println!(
+        "      |                     | total attack roll with bonuses: {}",
+        attack_roll_total
+    );
+    println!(
+        "      |                     | attack outcome: {:?}",
+        attack_outcome
+    );
+}
+
 pub fn debug_attack_roll_event(mut attack_roll_event_reader: EventReader<AttackRollEvent>) {
-    for event in attack_roll_event_reader.iter() {
-        println!("attack roll outcome: {:?}", event.attack_outcome);
+    let debug = true;
+    if debug {
+        for event in attack_roll_event_reader.iter() {
+            println!("attack roll outcome: {:?}", event.attack_outcome);
+        }
     }
 }
